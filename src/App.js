c61c519e280e88f763216b1bb1a8557f05d0aeca
swierzy89 <pswierzko@gmail.com>
2019-05-11 23:28:02 +0200

    Basic functionality of adding todo, todos stored in state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showAddTodo, setShowAddTodo] = useState(false);
+    const [text, setText] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!text.length) {
+            return;
+        }
+        const todo = {
+            text: text,
+            completed: false
+        };
+        setTodos([...todos, todo]);
+        setText('');
+    }
+
+    function handleChange(e) {
+        setText(e.target.value);
+    }
+
+    function handleClick(e) {
+        setShowAddTodo(true);
+    }
+
+    return (
+        <div className="ui container center aligned">
+            {showAddTodo ? (
+                <div>
+                    <form className="ui form" onSubmit={handleSubmit}>
+                        <div className="ui action input">
+                            <input type="text" placeholder="New todo" value={text} onChange={handleChange}/>
+                            <button className="ui button" type="submit">Add</button>
+                        </div>
+                    </form>
+                    {todos.length > 0 ? (
+                        <div className="ui list">
+                            {todos.map(todo => (
+                                <div className="item">{todo.text}</div>
+                            ))}
+                        </div>
+                    ) : null}
+                </div>
+            ) : (
+                <div className="ui container">
+                    <h2 className="ui header">No todo yet</h2>
+                    <div className="sub header">Add todo to get started</div>
+                    <button className="ui inverted primary button" onClick={handleClick}>New todo</button>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
